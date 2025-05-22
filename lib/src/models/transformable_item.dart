2d6eaf366546511722ed60caf2997cdea71ec67b
Murilo Giordano <murilo@gologbook.com>
2025-05-22 16:24:21 -0300

    removed some unused properties from models

--- lib/src/models/transformable_item.dart
+++ lib/src/models/transformable_item.dart
@@ -9,16 +9,11 @@ class TransformableItem {
   // Changed from offset to relativeOffset (0.0-1.0 range). Center is middle of the container.
   Offset relativeOffset;
 
-  double containerAreaWidth;
-  double containerAreaHeight;
-
   TransformableItem({
     required this.id,
     required this.child,
     this.scale = 1.0,
     this.rotation = 0.0,
     this.relativeOffset = Offset.zero, // Position as percentage of container
-    this.containerAreaWidth = 1.0,
-    this.containerAreaHeight = 1.0,
   });
 }
