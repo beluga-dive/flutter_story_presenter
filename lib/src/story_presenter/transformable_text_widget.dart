0b1b62d7e64ca490e6d8fd539bc9abb15fdef070
Murilo Giordano <murilo@gologbook.com>
2025-05-26 18:14:36 -0300

    Added padding to text sticker element

--- lib/src/story_presenter/transformable_text_widget.dart
+++ lib/src/story_presenter/transformable_text_widget.dart
@@ -16,19 +16,23 @@ class TransformableTextWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      textScaler: TextScaler.noScaling,
-      style: TextStyle(
-        color: foregroundColor,
-        backgroundColor: backgroundColor,
-        fontWeight: fontWeight,
-        fontSize: 16.0,
-        package: "beluga_brand",
-        fontFamily: "Quicksand",
-        height: 1.1,
-        letterSpacing: 0.5,
-        inherit: false,
+    return Container(
+      color: backgroundColor,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: Text(
+        text,
+        textScaler: TextScaler.noScaling,
+        style: TextStyle(
+          color: foregroundColor,
+          backgroundColor: backgroundColor,
+          fontWeight: fontWeight,
+          fontSize: 16.0,
+          package: "beluga_brand",
+          fontFamily: "Quicksand",
+          height: 1.1,
+          letterSpacing: 0.5,
+          inherit: false,
+        ),
       ),
     );
   }
